61c4e5c94904b5e07b18e46059b319ce25da7dfa
JasonCacante <jason.cacante@gmail.com>
2021-09-21 23:23:19 -0500

    Creando articulos usando los modelos y el controlador, recuerda el formulario sabe a donde mandar la informacion ya que ruby on rails actua por convencion

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   get 'home/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenido", to: "home#index"
+  get "/articles/new", to: "articles#new"
+  post "/articles", to: "articles#create"
 end

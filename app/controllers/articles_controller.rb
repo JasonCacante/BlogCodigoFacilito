4c2d512a7c88e8b3f6dd7e15d0e2af2d6fb83200
JasonCacante <jason.cacante@gmail.com>
2021-09-24 19:18:06 -0500

    Se usan plantillas para no repetir codigos en las vistas además se añaden rutas para gestionar los articulos creados y se estiliza un poco lo que vemos con bootsrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,11 +1,36 @@
 class ArticlesController < ApplicationController
+    before_action :find_article, only: [:show, :edit, :update, :destroy]
     def new
         @article = Article.new
-        @article.title = "Demo"
     end
 
     def create
-        @article = Article.create(title: params[:article][:title])
+        @article = Article.create(title: params[:article][:title], content: params[:article][:content])
         render json: @article
     end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+        @article.update(title: params[:article][:title], content: params[:article][:content])
+        redirect_to @article
+    end
+
+    def destroy
+        @article.destroy
+        redirect_to root_path
+    end
+
+    def find_article
+        @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,13 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  root to: 'home#index'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get "/bienvenido", to: "home#index"
   get "/articles/new", to: "articles#new"
   post "/articles", to: "articles#create"
+  get "/articles/:id", to: "articles#show"
+  get "/articles/:id/edit", to: "articles#edit"
+  patch "/articles/:id", to: "articles#update", as: :article
+  delete "articles/:id", to: "articles#destroy"
+  get "/articles", to: "articles#index"
 end
